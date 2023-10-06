32c51f746f3f00c844ae024936b4b7c4f2be541f
MarcoAurelioAcosta <marcochongsu@fpuna.edu.py>
2023-10-06 14:30:42 -0300

    Opciones para el contenido
    Opciones de editar y eliminar contenido creado.

--- contenido/urls.py
+++ contenido/urls.py
@@ -5,5 +5,8 @@ app_name = 'contenido'
 urlpatterns = [
     path('crear/', views.crearContenido, name='crear_contenido'),
     path('lista/', views.listaContenido, name='lista_contenido'),
-    path('ver/<int:id>/', views.verContenido, name='ver_contenido')
+    path('ver/<int:id>/', views.verContenido, name='ver_contenido'),
+    path('editar/<int:id>/', views.editarContenido, name='editar_contenido'),
+    path('eliminar/<int:id>/', views.eliminarContenido, name='eliminar_contenido'),
+    path('eliminar/<int:id>/confirmar/', views.confirmarEliminarContenido, name='confirmar_eliminar_contenido'),
 ]
--- contenido/views.py
+++ contenido/views.py
@@ -3,6 +3,7 @@ from .models import Contenido
 from .forms import ContenidoForm
 from usuario.models import UserCategoria
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 
 @login_required
 def crearContenido(request):
@@ -19,6 +20,33 @@ def crearContenido(request):
         form = ContenidoForm(categorias_autor=categorias_autor, autor=request.user)
     
     return render(request, 'contenido/crearContenido.html', {'form': form})
+
+
+@login_required
+def editarContenido(request, id):
+    categorias_autor = UserCategoria.objects.filter(user=request.user, rol__nombre='Autor').values_list('categoria__id', flat=True)
+    contenido = get_object_or_404(Contenido, id=id)
+    if request.method == 'POST':
+        form = ContenidoForm(request.POST, instance=contenido, autor=request.user, categorias_autor=categorias_autor)
+        if form.is_valid():
+            form.save()
+            return redirect('contenido:lista_contenido')
+    else:
+        form = ContenidoForm(instance=contenido, categorias_autor=categorias_autor, autor=request.user)
+    return render(request, 'contenido/editarContenido.html', {'form': form})
+
+
+@login_required
+def eliminarContenido(request, id):
+    contenido = get_object_or_404(Contenido, id=id)
+    contenido.delete()
+    return redirect('contenido:lista_contenido')
+
+
+@login_required
+def confirmarEliminarContenido(request, id):
+    contenido = get_object_or_404(Contenido, id=id)
+    return render(request, 'contenido/confirmarEliminarContenido.html', {'contenido': contenido})
 
 
 def listaContenido(request):
