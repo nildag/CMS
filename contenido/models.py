1b969af64576110f8fe2002e3c385da51e2aed62
nthprt <nathalyprieto801@fpuna.edu.py>
2023-10-20 10:12:21 -0300

    Contenido: Docstrings

--- contenido/models.py
+++ contenido/models.py
@@ -7,16 +7,23 @@ from django.conf import settings  # Importa la configuración de Django
 from django.contrib.auth.models import User
 
 class Contenido(models.Model):
-    """
-    Modelo para almacenar el contenido creado por los usuarios.
+    class Contenido(models.Model):
+        """
+        Modelo para almacenar el contenido creado por los usuarios.
 
-    Atributos:
-        titulo (str): Título del contenido.
-        cuerpo (RichTextField): Cuerpo del contenido en formato rico.
-        autor (User): Usuario que crea el contenido.
-        categoria (Categoria): Categoría a la que pertenece el contenido.
-        fecha_creacion (datetime): Fecha y hora de creación del contenido.
-    """
+        Atributos:
+            - titulo (str): Título del contenido.
+            - cuerpo (RichTextField): Cuerpo del contenido en formato rico.
+            - autor (User): Usuario que crea el contenido.
+            - categoria (Categoria): Categoría a la que pertenece el contenido.
+            - fecha_creacion (datetime): Fecha y hora de creación del contenido.
+            - puntuacion (Decimal): Puntuación promedio del contenido.
+            - numero_valoraciones (int): Número total de valoraciones del contenido.
+
+        Métodos:
+            - for_user(user): Devuelve los contenidos creados por un usuario específico.
+            - for_categorias(categorias): Devuelve los contenidos asociados a una lista de categorías.
+        """
     titulo = models.CharField(max_length=30, default="titulo")
     cuerpo = RichTextField(default="cuerpo")
     #autor = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
@@ -36,11 +43,18 @@ class Contenido(models.Model):
 
     @classmethod
     def for_user(self, user):
-
         """
-        Funcion que devuelve los contenidos de un usuario
-        :param user: usuario a comprobar (User)
-        :return: lista de contenidos del usuario
+        Devuelve una lista de contenidos creados por un usuario específico.
+
+        Args:
+            user (User): El usuario para el que se obtienen los contenidos.
+
+        Returns:
+            QuerySet: Una lista de objetos de contenido creados por el usuario.
+
+        Example:
+            Para obtener los contenidos creados por el usuario 'alice', puedes usar:
+            contenidos = Contenido.for_user(User.objects.get(username='alice'))
         """
 
         return Contenido.objects.filter(autor=user)
@@ -57,9 +71,33 @@ class Contenido(models.Model):
         return Contenido.objects.filter(categoria__in=categorias)
 
 class Valoracion(models.Model):
+    """
+    Modelo que representa una valoración de contenido.
+
+    Atributos:
+        contenido (Contenido): El contenido que se valora.
+        usuario (User): El usuario que realiza la valoración.
+        puntuacion (int): La puntuación otorgada, generalmente en un rango de 1 a 5.
+        fecha (datetime): La fecha y hora en que se creó la valoración.
+
+    Métodos:
+        __str__(): Devuelve una representación legible de la valoración.
+
+    Atributos:
+        contenido (Contenido): El contenido que se valora.
+        usuario (User): El usuario que realiza la valoración.
+        puntuacion (int): La puntuación otorgada, generalmente en un rango de 1 a 5.
+        fecha (datetime): La fecha y hora en que se creó la valoración.
+    """
+
     contenido = models.ForeignKey(Contenido, on_delete=models.CASCADE)
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     puntuacion = models.PositiveIntegerField()  # Aquí puedes usar un rango de 1 a 5, por ejemplo
     fecha = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
+        """
+        Devuelve una representación legible de la valoración.
+        :return: Cadena con el formato "Valoración de [nombre de usuario] para [título del contenido]".
+        """
         return f"Valoración de {self.usuario.username} para {self.contenido.titulo}"
--- contenido/test_views.py
+++ contenido/test_views.py
@@ -7,26 +7,54 @@ from contenido.forms import ContenidoForm
 
 
 class EditarContenidoViewTest(TestCase):
+    """
+    Esta clase contiene pruebas para la vista de edición de contenido.
+
+    Atributos de Clase:
+        user (User): Un usuario de prueba para realizar las pruebas.
+        categoria (Categoria): Una categoría de prueba para el contenido.
+        contenido (Contenido): Un contenido de prueba para las pruebas.
+
+    Métodos de Clase:
+        setUpTestData(): Configura los datos de prueba.
+        setUp(): Configura el entorno de prueba.
+
+    Métodos de Prueba:
+        test_editar_contenido_view_GET(): Prueba la solicitud GET a la vista de edición.
+        test_editar_contenido_view_POST_valid(): Prueba la solicitud POST con datos válidos.
+        test_editar_contenido_view_POST_invalid(): Prueba la solicitud POST con datos no válidos.
+    """
+
     @classmethod
     def setUpTestData(cls):
-# Crear un usuario usando el modelo de usuario personalizado
+        """
+        Configura los datos de prueba.
+        """
+        # Crear un usuario usando el modelo de usuario personalizado
         cls.user = User.objects.create_user(username='testuser', password='12345')
-       # Crear una categoría
+        # Crear una categoría
         cls.categoria = Categoria.objects.create(nombre='test categoria')
-       # Crear un contenido
+        # Crear un contenido
         cls.contenido = Contenido.objects.create(titulo='test titulo', cuerpo='test cuerpo', autor=cls.user, categoria=cls.categoria)
 
     def setUp(self):
-
+        """
+        Configura el entorno de prueba.
+        """
         self.client = Client()
         self.client.login(username='testuser', password='12345')
 
     def test_editar_contenido_view_GET(self):
+        """
+        Prueba la solicitud GET a la vista de edición.
+        """
         # Emitir una solicitud GET a la vista
         response = self.client.get(reverse('contenido:editar_contenido', args=[self.contenido.id]))
 
-
     def test_editar_contenido_view_POST_valid(self):
+        """
+        Prueba la solicitud POST con datos válidos.
+        """
         # Emitir una solicitud POST a la vista con datos válidos
         response = self.client.post(reverse('contenido:editar_contenido', args=[self.contenido.id]), {
             'titulo': 'test titulo editado',
@@ -34,16 +62,18 @@ class EditarContenidoViewTest(TestCase):
             'categoria': self.categoria.id
         })
 
-
     def test_editar_contenido_view_POST_invalid(self):
-       # Emitir una solicitud POST a la vista con datos no válidos
+        """
+        Prueba la solicitud POST con datos no válidos.
+        """
+        # Emitir una solicitud POST a la vista con datos no válidos
         response = self.client.post(reverse('contenido:editar_contenido', args=[self.contenido.id]), {
             'titulo': '',
             'cuerpo': 'test cuerpo editado',
             'categoria': self.categoria.id
         })
-       # Verifique que el código de estado de respuesta sea 200
+        # Verifique que el código de estado de respuesta sea 200
         self.assertEqual(response.status_code, 200)
-      # Comprobar que el formulario se muestra con errores
+        # Comprobar que el formulario se muestra con errores
         self.assertIsInstance(response.context['form'], ContenidoForm)
         self.assertTrue(response.context['form'].errors)
