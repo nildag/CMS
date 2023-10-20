529c7e97482ab81e8e0290bc7dfdecb3681a1970
nthprt <nathalyprieto801@fpuna.edu.py>
2023-10-20 10:13:58 -0300

    Rol: Docstrings

--- rol/views.py
+++ rol/views.py
@@ -4,19 +4,26 @@ from .forms import RolForm
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import login_required
 
+
 def user_admin_roles(user):
-
     """
-    Funcion que comprueba si el usuario es administrador de roles (tiene el permiso "Administrar roles")
-    :param user: usuario a comprobar (User)
-    :return: True si es administrador de roles, False en caso contrario
-    """
+    Función que comprueba si el usuario es administrador de roles (tiene el permiso "Administrar roles").
 
+    :param user: El usuario a comprobar (User).
+    :return: True si es administrador de roles, False en caso contrario.
+    """
     return user.admin_roles
+
 
 @login_required
 @user_passes_test(user_admin_roles)
 def crear_rol(request):
+    """
+    Vista para crear un nuevo rol.
+
+    :param request: La solicitud HTTP.
+    :return: La página de creación de roles o redirecciona a la lista de roles si se crea con éxito.
+    """
     if request.method == 'POST':
         form = RolForm(request.POST)
         if form.is_valid():
@@ -26,16 +33,31 @@ def crear_rol(request):
         form = RolForm()
     return render(request, 'rol/crearRol.html', {'form': form})
 
+
 @login_required
 @user_passes_test(user_admin_roles)
 def lista_roles(request):
+    """
+    Vista para mostrar la lista de roles.
+
+    :param request: La solicitud HTTP.
+    :return: La página de lista de roles.
+    """
     roles = Rol.getAll()
     roles = roles.order_by('-last_modification', 'nombre')
     return render(request, 'rol/lista_roles.html', {'roles': roles})
 
+
 @login_required
 @user_passes_test(user_admin_roles)
 def borrar_rol(request, rol_id):
+    """
+    Vista para eliminar un rol.
+
+    :param request: La solicitud HTTP.
+    :param rol_id: El ID del rol a eliminar.
+    :return: La página de lista de roles o redirecciona a la lista de roles si se elimina con éxito.
+    """
     rol = get_object_or_404(Rol, pk=rol_id)
 
     if request.method == 'POST':
@@ -44,19 +66,35 @@ def borrar_rol(request, rol_id):
 
     return render(request, 'rol/lista_roles.html', {'roles': Rol.objects.all()})
 
+
 @login_required
 @user_passes_test(user_admin_roles)
 def lista_permisos(request, rol_id):
+    """
+    Vista para mostrar los permisos de un rol.
+
+    :param request: La solicitud HTTP.
+    :param rol_id: El ID del rol cuyos permisos se mostrarán.
+    :return: La página de lista de permisos de un rol.
+    """
     rol = get_object_or_404(Rol, pk=rol_id)
     return render(request, 'rol/lista_permisos.html', {'rol': rol})
+
 
 @login_required
 @user_passes_test(user_admin_roles)
 def editar_rol(request, rol_id):
+    """
+    Vista para editar un rol existente.
+
+    :param request: La solicitud HTTP.
+    :param rol_id: El ID del rol a editar.
+    :return: La página de edición de roles o redirecciona a la lista de roles si se edita con éxito.
+    """
     rol = get_object_or_404(Rol, pk=rol_id)
     if request.method == 'POST':
         form = RolForm(request.POST, instance=rol)
-        if form.is_valid():
+        if form. is_valid():
             form.save()
             return redirect('rol:lista_roles')
     else:
